d8b2d44ce5ec3807fdad9a43da1bc90bff885bf3
ItonRomero <jaysonromero1218@gmail.com>
2022-12-20 19:17:29 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Article, Brand, CTA, Feature, Navbar } from './components'
+
+function App() {
+
+  return (
+    <div>
+      <Article/>
+      <Brand/>
+      <CTA/>
+      <Feature/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
